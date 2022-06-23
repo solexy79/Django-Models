9aa5acb7772eef57b20b8046411436312a0e8846
solexy79 <agboolasola6@gmail.com>
2022-06-23 04:06:36 +0100

    added blog app

--- I4G016412UQX/blog/models.py
+++ I4G016412UQX/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200)
+    text = models.CharField()
+    author = models.ForeignKey(get_user_model())
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
